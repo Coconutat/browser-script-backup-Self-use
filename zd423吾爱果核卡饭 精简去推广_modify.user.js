2ac32c7ada93b1aca1a6c449d962fbb24017d692
Coconut <85353552+Coconutat@users.noreply.github.com>
2022-08-10 22:25:22 +0800

    Update zd423吾爱果核卡饭 精简去推广_modify.user.js
    update new version

--- zd423吾爱果核卡饭 精简去推广_modify.user.js
+++ zd423吾爱果核卡饭 精简去推广_modify.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         zd423|吾爱|果核|卡饭 精简去推广——Cocoontat修改版
 // @namespace    https://github.com/Coconutat/browser-script-backup-Self-use
-// @version      0.9.8.12.1
+// @version      0.9.8.12.2
 // @description  页面精简去推广内容只留主内容，卡饭自动签到等功能，殁漂遥去推广请看介绍，不在此脚本
 // @author       Aerozb，修改 by Cocoontat
 // @match        http://bbs.zhiyoo.net/search.php?mod=forum&adv=yes
