68b8c620bb9a993ea73bba7b445951cd4dee4d1c
Coconut <coconutat@protonmail.com>
2024-01-10 11:21:49 +0800

    423down:remove new ad

--- zd423吾爱果核卡饭 精简去推广_modify.user.js
+++ zd423吾爱果核卡饭 精简去推广_modify.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         zd423|吾爱|果核|卡饭 精简去推广——Cocoontat修改版
 // @namespace    https://github.com/Coconutat/browser-script-backup-Self-use/zd423吾爱果核卡饭 精简去推广_modify.js
-// @version      0.9.8.12.6
+// @version      0.9.8.12.7
 // @description  页面精简去推广内容只留主内容，卡饭自动签到等功能，殁漂遥去推广请看介绍，不在此脚本
 // @author       Aerozb，修改 by Cocoontat
 // @match        http://bbs.zhiyoo.net/search.php?mod=forum&adv=yes
@@ -29,19 +29,21 @@
     //加入‘限时’字符，去除新的广告 2022.08
     //加入‘纯净尊享’，‘免费纯净’字符，‘免费的资源搜索神器’字符，去除新的广告 2022.10
     //加入‘纯净尊享’，‘免费尊享版’字符，去除新的广告 2022.11
+    //加入‘产品推广’字符，去除新的广告 2024.01
     if (url.indexOf('423down') != -1) {
         document.querySelector("#hasfixed > div.wrapper > div.sidebar > div:nth-child(2)").remove();
         let ul = document.querySelector("#hasfixed > div.wrapper > div.content-wrap > div > ul").children;
         for (let i = ul.length - 1; i > -1; i--) {
             let a = ul[i].querySelectorAll('a');
             for (let j = a.length - 1; j > -1; j--) {
-                if (a[j].innerText.indexOf('默认分类') != -1||a[j].innerText.indexOf('优惠') != -1 ||a[j].innerText.indexOf('推荐') != -1 || a[j].innerText.indexOf('限时') != -1 || a[j].innerText.indexOf('纯净尊享') != -1 || a[j].innerText.indexOf('免费纯净') != -1 || a[j].innerText.indexOf('免费的资源搜索神器') != -1 || a[j].innerText.indexOf('免费尊享') != -1) {
+                if (a[j].innerText.indexOf('默认分类') != -1 || a[j].innerText.indexOf('优惠') != -1 || a[j].innerText.indexOf('推荐') != -1 || a[j].innerText.indexOf('限时') != -1 || a[j].innerText.indexOf('纯净尊享') != -1 || a[j].innerText.indexOf('免费纯净') != -1 || a[j].innerText.indexOf('免费的资源搜索神器') != -1 || a[j].innerText.indexOf('免费尊享') != -1 || a[j].innerText.indexOf('产品推广') != -1) {
                     ul[i].remove();
                     break;
                 }
             }
         }
     }
+
     //智友搜索按发布时间排序,精简多余内容
     else if (url.indexOf('http://bbs.zhiyoo.net/search') != -1) {
         document.querySelector("#scform > tbody > tr > td:nth-child(1) > h1 > img").remove();
