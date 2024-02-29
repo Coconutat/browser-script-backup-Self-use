37f9a6ea98d89e9368b4c5bd391f6403ac43c880
Coconut <85353552+Coconutat@users.noreply.github.com>
2024-02-29 13:54:55 +0800

    423down mobile ad remover:delay 3s then remove ad

--- 423down_mobile_ad_remove.user.js
+++ 423down_mobile_ad_remove.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         zd423(423down)移动版精简去推广
 // @namespace    https://github.com/Coconutat/browser-script-backup-Self-use/423down_mobile_ad_remove.user.js
-// @version      0.1.alpha
+// @version      0.1.beta
 // @description  zd423(423down)移动版精简去推广
 // @author       by Cocoontat
 // @match        https://www.423down.com/*
@@ -15,17 +15,18 @@
 
     let url = window.location.href;
 
-    if (url.indexOf('423down') != -1) {
-        let li = document.querySelector("div#wrap > div.archive-list > ul.post-list").children;
-        for (let i = li.length - 1; i > -1; i--) {
-            let a = li[i].querySelectorAll('span');
-            for (let j = a.length - 1; j > -1; j--) {
-                if (a[j].innerText.indexOf('置顶') != -1){
-                    li[i].remove();
-                    break;
+    setTimeout(function () {
+        if (url.indexOf('423down') != -1) {
+            let li = document.querySelector("div#wrap > div.archive-list > ul.post-list").children;
+            for (let i = li.length - 1; i > -1; i--) {
+                let a = li[i].querySelectorAll('span');
+                for (let j = a.length - 1; j > -1; j--) {
+                    if (a[j].innerText.indexOf('置顶') != -1){
+                        li[i].remove();
+                        break;
+                    }
                 }
             }
         }
-
-    }
+    }, 3000); // 2000 milliseconds (2 seconds) delay
   })();
